9ec6876ca567bae7eb36d255a269f1dfc3049a5d
Danilo Ferreira <danilocsf@gmail.com>
2025-07-05 11:55:43 -0300

    Estrutura inicial

---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/config/web/StringToTSIDConverter.java
@@ -0,0 +1,14 @@
+package com.algaworks.algasensors.temperature.processing.api.config.web;
+
+import io.hypersistence.tsid.TSID;
+import lombok.NonNull;
+import org.springframework.core.convert.converter.Converter;
+
+
+public class StringToTSIDConverter implements Converter<String, TSID> {
+
+    @Override
+    public TSID convert(@NonNull String source) {
+        return TSID.from(source);
+    }
+}
