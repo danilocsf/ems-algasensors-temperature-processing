80becae77243119861ed625ff958b9e8096f893d
Danilo Ferreira <danilocsf@gmail.com>
2025-10-11 15:04:20 -0300

    Publicação de mensagens

--- src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -2,8 +2,11 @@ package com.algaworks.algasensors.temperature.processing.api.controller;
 
 import com.algaworks.algasensors.temperature.processing.api.model.TemperatureLogOutput;
 import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
+import com.algaworks.algasensors.temperature.processing.infrasctructure.rabbitmq.RabbitMQConfig;
 import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +17,10 @@ import java.time.OffsetDateTime;
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 @Slf4j
+@RequiredArgsConstructor
 public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public void data(@PathVariable TSID sensorId, @RequestBody String input) {
@@ -34,6 +40,11 @@ public class TemperatureProcessingController {
                 .registeredAt(OffsetDateTime.now())
                 .build();
         log.info(logOutput.toString());
+
+        String routingKey = "";
+        Object payload = logOutput;
+
+        rabbitTemplate.convertAndSend(RabbitMQConfig.FANOUT_EXCHANGE_NAME, routingKey, payload);
     }
 
 }
